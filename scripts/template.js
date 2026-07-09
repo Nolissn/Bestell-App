bec8705fc6be4702af7647bf84b9171b6214e705
Luis Simon <nolissn22222@gmail.com>
2026-07-09 12:16:53 +0200

    fix: made the burger_house_brand_icon absolute positioning relative to the hero_image, instead of the body to ensure vertical responsive behavior. Also added basic layout for the food cards

--- scripts/template.js
+++ scripts/template.js
@@ -1,15 +1,17 @@
 function getFoodCardTemplate(food, category, index) {
   return `
     <div class="food_card">
-      <img class="food_card_image" src="${food.image}" alt="${food.name}" />
-      <div class="food_card_text">
+      <img class="food_card_image" src="${food.image}" alt="${food.name}"/>
+      <div>
         <h3 class="food_card_name">${food.name}</h3>
         <p class="food_card_description">${food.description}</p>
-        <span class="food_card_price">${food.price}</span>
       </div>
-      <button class="food_card_add_button" onclick="addToCart('${category}', ${index})">
-        +
+      <div class="food_card_price_and_button_layout_div">
+      <span class="food_card_price">${food.price}</span>
+      <button class="food_card_button" onclick="addToBasket('${category}', ${index})">
+        Add to basket
       </button>
+      </div>
     </div>
   `;
 }
